46cadb20752d137ef0650779e18f401f9bb12c3c
ziyagasimov <ziyagasimov0@gmail.com>
2026-03-22 20:42:46 +0400

    last tasks

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,18 +1,28 @@
+#!/usr/bin/python3
+""" FizzBuzz """
+import sys
+
 def fizzbuzz(n):
     if n < 1:
         return
 
     tmp_result = []
     for i in range(1, n + 1):
-        # Check the combined condition FIRST
+        # 1. Check for BOTH first (or use i % 15 == 0)
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        # 2. Then check individual cases
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
-    
-    # Using print(*tmp_result) is also a neat way to print space-separated values
+            
     print(" ".join(tmp_result))
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        sys.exit(1)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
